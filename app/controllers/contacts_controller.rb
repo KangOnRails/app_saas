c1f7b4e8d5d42ea23c33870a0ee71dd75bc644b0
Dru Abraham <D24Abraham@gmail.com>
2016-07-03 07:04:34 +0000

    Added validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,21 @@ class ContactsController < ApplicationController
     end
 
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Message Sent" # Flash Notice 
+            redirect_to new_contact_path
+        else 
+            flash[:danger] = "Message not sent ID10T ERROR occured"
+            redirect_to new_contact_path
+            
+        end
     end
 end
+
+
+private
+    def contact_params
+         params.require(:contact).permit(:name, :email, :comments) # White list the name email and comment for security purposes
+    end
