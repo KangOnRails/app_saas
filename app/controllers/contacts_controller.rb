92c2392d1365ac54b62062c92031f097571720f8
Dru Abraham <D24Abraham@gmail.com>
2016-07-03 11:36:54 +0000

    Fixed and issue

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
         if @contact.save
             name = params[:contact][:name]
             email = params[:contact][:email]
-            body = params [:contact][:comments]
+            body = params[:contact][:comments]
             
             ContactMailer.comtact_email(name, email, body).deliver
             
